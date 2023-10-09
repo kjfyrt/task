d2c806b601d6bf9ce9ecb17e51372777913a94f0
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-10-09 09:50:01 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b = 0;
+    cin >> a;
+    if (a<0){
+        a = -a;
+    }
+    if (a == 0){
+        return 0;
+    }
+    if (a > 0){
+        while (a != 0){
+            if ((a%2) == 1){
+                b = b + (a%10);
+            }
+            a = a / 10;
+        }
+    }
+    return b;
+}
