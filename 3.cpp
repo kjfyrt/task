66295f29644cc7a89812dbcf42b5d3fb510ff14f
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-10-09 10:35:03 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, max = 0, max2 = 0;
+    cin >> a;
+    if (a < 0){
+        a = -a;
+    }
+    if (a < 10){
+        cout << 0;
+    }
+    while(a){
+        if (a%10 >= max){
+            max2 = max;
+            max = a%10;
+        }
+        if ((max2 < max) and (max > a%10)){
+            max2 = a%10;
+        }
+        a /= 10;
+    }
+    cout << max*max2 << endl;
+}
